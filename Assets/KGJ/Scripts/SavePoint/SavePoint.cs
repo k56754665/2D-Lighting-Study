e313bc367571949465a103d6eb57b05b3ca387be
k56754665 <95170766+k56754665@users.noreply.github.com>
2025-04-20 20:00:56 +0900

    [FEAT] 세이브 포인트 프리팹 수정

--- Assets/KGJ/Scripts/SavePoint/SavePoint.cs
+++ Assets/KGJ/Scripts/SavePoint/SavePoint.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class SavePoint : MonoBehaviour
 {
     [SerializeField] private int savePointIndex; // 고유 인덱스 public Vector3 Position => transform.position; // 세이브 포인트 위치 public int Index => savePointIndex;
+    GameObject _prefab;
+
+    private void Start()
+    {
+        _prefab = Resources.Load<GameObject>("Particles/GetYou_Particle");
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -10,6 +16,8 @@ public class SavePoint : MonoBehaviour
         {
             SaveManager.Instance.SaveGame(savePointIndex, transform.position);
             Debug.Log($"세이브 포인트 {savePointIndex} 저장됨!");
+            Instantiate(_prefab, transform.position, Quaternion.identity);
+            Destroy(gameObject); // 세이브 포인트에 들어가면 오브젝트 삭제
         }
     }
 
