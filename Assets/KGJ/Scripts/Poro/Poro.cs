81e324c7387fa10874b8138739cca55d091d1019
k56754665 <95170766+k56754665@users.noreply.github.com>
2025-04-21 11:04:11 +0900

    [FEAT] 보스 사망 애니메이션 추가

--- Assets/KGJ/Scripts/Poro/Poro.cs
+++ Assets/KGJ/Scripts/Poro/Poro.cs
@@ -8,20 +8,18 @@ public class Poro : MonoBehaviour
     GameObject _blood;
     public Action<int> OnPoroHitEvent;
     UI_KillEnding _killEndingUI;
+    Animator _animator;
 
     void Start()
     {
         _poroDialog = transform.GetChild(0).GetComponent<PoroDialog>();
         _blood = Resources.Load<GameObject>("Prefabs/KGJ/Blood");
         _killEndingUI = FindAnyObjectByType<UI_KillEnding>();
+        _animator = GetComponent<Animator>();
     }
 
     public void TakeDamage()
     {
-        Vector2 randomOffset = UnityEngine.Random.insideUnitCircle * 1f; // 1f 반경 원 안의 랜덤 위치
-        Vector3 spawnPosition = transform.position + new Vector3(randomOffset.x, randomOffset.y, 0);
-        Instantiate(_blood, spawnPosition, transform.rotation);
-
         _poroDialog.Shoot();
         hp--;
         OnPoroHitEvent?.Invoke(hp);
@@ -30,7 +28,14 @@ public class Poro : MonoBehaviour
             // TODO : 디버그 로그 양식에 맞게 수정 필요
             _killEndingUI.TurnOn();
             Debug.Log("Kill Ending");
-            Destroy(gameObject);
+            _animator.Play("PoroDie");
+            Destroy(transform.GetChild(0).gameObject);
+        }
+        else
+        {
+            Vector2 randomOffset = UnityEngine.Random.insideUnitCircle * 1f; // 1f 반경 원 안의 랜덤 위치
+            Vector3 spawnPosition = transform.position + new Vector3(randomOffset.x, randomOffset.y, 0);
+            Instantiate(_blood, spawnPosition, transform.rotation);
         }
     }
 
